fed523b17bdcf324ad4f2de1fe139af808b15a24
Stas <stazzzdzr@gmail.com>
2021-08-31 01:13:48 +0300

    menu new started"

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,39 +1,39 @@
+@@include('wow.min.js');
 @@include('swiper-bundle.min.js');
 
 //===swiper slider===
-
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	breakpoints: {
 		320: {
-		  slidesPerView: 1,
-		  initialSlide: 4,
-		  spaceBetween: 0,
-		  spaceBetween: 30,
+			slidesPerView: 1,
+			initialSlide: 4,
+			spaceBetween: 0,
+			spaceBetween: 30,
 		},
 		1350: {
-		  slidesPerView: 3,
-		  spaceBetween: 30,
+			slidesPerView: 3,
+			spaceBetween: 30,
 		}
 	},
 	direction: 'horizontal',
 	loop: true,
 	centeredSlides: true,
 	initialSlide: 1,
- //Расстояние между слайдами в пикселях.
-	//delay: 400,
-	
-	// autoplay: {
-	// 	delay: 4000,
-	//  },
-
+	//Расстояние между слайдами в пикселях.
+	delay: 400,
+	autoplay: {
+		delay: 3000,
+	 },
 	// Navigation arrows
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
 	},
+});
 
- });
+//======wow========
+new WOW().init();
 
 //main Tab function
 const tabs = document.querySelectorAll('.tabs-section__tab-nav-item');
@@ -51,10 +51,10 @@ tabs.forEach((tab) => {
 })
 
 function addActiveClass(target, buttons, ...anotherClasses) {
-	buttons.forEach((button, numButton)=> {
+	buttons.forEach((button, numButton) => {
 		if (target === button) {
 			buttons[numButton].classList.add('active')
-			anotherClasses.forEach(elem=>{
+			anotherClasses.forEach(elem => {
 				elem[numButton].classList.add('active')
 			})
 		}
@@ -62,7 +62,8 @@ function addActiveClass(target, buttons, ...anotherClasses) {
 }
 
 function deleteActiveClass(...deleteActiveClassAllElements) {
-	deleteActiveClassAllElements.forEach(nodes => { nodes.forEach(elem=> {
+	deleteActiveClassAllElements.forEach(nodes => {
+		nodes.forEach(elem => {
 			if (elem.classList.contains('active')) elem.classList.remove('active')
 		})
 	})
@@ -70,7 +71,7 @@ function deleteActiveClass(...deleteActiveClassAllElements) {
 
 
 
- //spoiler
+//=============spoiler========
 
 const spoilers = document.querySelectorAll('.questions__item-wrapper')
 spoilers.forEach(e => e.addEventListener('click', (e) => spoiler(e.target)))
@@ -84,6 +85,40 @@ const spoiler = (eventTarget) => {
 
 	spoilerBody.classList.toggle('active')
 	spoilerBtn.classList.toggle('active')
-
-
 }
+
+//==============modal==============
+
+const modalActive = () => {
+	const modal = document.querySelector('.overlay')
+	const body = document.querySelector('body')
+
+	modal.classList.add('active')
+	body.classList.add('body-lock')
+
+	const modalClose = () => {
+		modal.classList.remove('active')
+		body.classList.remove('body-lock')
+	}
+	document.querySelector('.modal__close').onclick = modalClose;
+
+	
+}
+
+document.querySelector('.mobile-app__info-get-course').onclick = modalActive;
+
+
+//=============Navigation menu==============
+
+//forTeacher education
+
+const toggleClassFunc = function () {
+	document.querySelector('#forTeacherOpen').classList.toggle('open')
+}
+const toggleClassFunc1 = function () {
+	document.querySelector('#educationOpen').classList.toggle('open')
+}
+
+
+document.querySelector('#education').onclick = toggleClassFunc1
+document.querySelector('#forTeacher').onclick = toggleClassFunc
